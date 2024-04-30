b2952d191ae6cf2103c6164175a7f3e3ccd44fe9
living-wonder <132438390+living-wonder@users.noreply.github.com>
2024-04-30 19:17:49 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (47.51%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,14 @@
+// Space: O(1)
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int maxDiff = 0, minDiff = 0;
+        for (char c : s) {
+            maxDiff += (c == '(' || c == '*') ? 1 : -1;
+            minDiff += (c == ')' || c == '*') ? -1 : 1;
+            if (maxDiff < 0) return false;
+            minDiff = max(0, minDiff);
+        }
+        return minDiff == 0;
+    }
+};
